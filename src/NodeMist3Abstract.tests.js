c9b60e58481541f85834d921117c4d91863807f8
Davi Ortega <daviortega@users.noreply.github.com>
2018-07-04 17:06:05 -0700

    Make fasta refactor (#16)
    * adding a more informative error to info
    
    * 1.4.5
    
    * handling errors in addAseqInfo and make config of mocha reporter for bunyan
    
    * 1.4.7
    
    * Handling null sequences in MakeFasta and fixing errors
    
    * 1.4.8
    
    * partial work towards multi api list
    
    * better error handling
    
    * better error handling
    
    * caltech api
    
    * refactor of MakeFasta
    
    * 1.5.0

---
+++ src/NodeMist3Abstract.tests.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+
+chai.use(chaiAsPromised)
+
+const expect = chai.expect
+const should = chai.should()
+
+const NodeMist3Abstract = require('./NodeMist3Abstract')
+
+describe.skip('NodeMist3Abstract')
