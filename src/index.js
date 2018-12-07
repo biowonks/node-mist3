a939be5bea2480aa0f49c1bdef3f070ecf53295d
Davi Ortega <ortegad@caltech.edu>
2018-12-06 18:27:34 -0800

    fixing index.js for new class

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@
 const Genes = require('./Genes')
 const MakeFasta = require('./MakeFasta')
 const Genomes = require('./Genomes')
+const Taxonomy = require('./Taxonomy')
 
 module.exports = {
 	Genes,
 	Genomes,
-	MakeFasta
+	MakeFasta,
+	Taxonomy
 }
